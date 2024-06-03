de9abb78c42fe0e5571aca6a96981257e5229c16
AnggeldDAV <anggeld.adav@gmail.com>
2024-06-03 14:22:42 +0200

    Estilos

--- PruebaMVC/wwwroot/js/site.js
+++ PruebaMVC/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Plugin Imagenes
+    new SimpleLightbox({
+        elements: '#grupos a.grupos-box'
+    });
+
+});
