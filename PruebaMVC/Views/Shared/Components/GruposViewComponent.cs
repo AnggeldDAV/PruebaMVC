840094691023014e656a224733e4a27d843061f4
AnggeldDAV <anggeld.adav@gmail.com>
2024-06-12 09:24:25 +0200

    ViewComponent Artista

---
+++ PruebaMVC/Views/Shared/Components/GruposViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaMVC.Models;
+using PruebaMVC.Services.Repositorio;
+using PruebaMVC.Services.Speficification;
+
+namespace PruebaMVC.Views.Shared.Components
+{
+    public class GruposViewComponent(IGenericRepositorio<VistaGruposArtista> coleccion) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int ArtistaId)
+        {
+            var items = await coleccion.DameTodos();
+            IArtistaSpecification especificacion = new ArtistaAutentification(ArtistaId);
+            var itemsFiltrados = items.Where(especificacion.IsValid);
+            return View(itemsFiltrados);
+        }
+    }
+}
